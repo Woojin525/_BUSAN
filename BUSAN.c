6c858afa4fb462bc4fd95906464c9f8b68377d03
cwj2020 <20202464@gm.hannam.ac.kr>
2024-05-28 13:26:01 +0900

    2-1 정리

--- BUSAN.c
+++ BUSAN.c
@@ -36,47 +36,25 @@ int main() {
 
     printf("---------------\n");
 
-    //기차 상단
-    for (int i = 0; i < LEN; i++) {
+    //기차
+    for (int i = 0; i < LEN; i++)
         printf("#");
-    }
-    printf("\n");
-
-    //기차 중단
-    printf("#");
-
-    //C위치
-    for (int i = 0; i < C - 1; i++) {
+    printf("\n#");
+    for (int i = 0; i < C - 1; i++)
         printf(" ");
-    }
     printf("C");
-
-    //Z위치
-    for (int i = C; i < Z - 1; i++) {
+    for (int i = C; i < Z - 1; i++)
         printf(" ");
-    }
     printf("Z");
-
-    //M위치
-    for (int i = Z; i < M - 1; i++) {
+    for (int i = Z; i < M - 1; i++)
         printf(" ");
-    }
     printf("M");
-
-
-    //우측#위치
-    for (int i = 0; i <= LEN - M - 4; i++) {
+    for (int i = 0; i <= LEN - M - 3; i++)
         printf(" ");
-    }
     printf("#\n");
-
-    //기차 하단
-    for (int i = 0; i < LEN; i++) {
+    for (int i = 0; i < LEN; i++)
         printf("#");
-    }
-    printf("\n");
-    printf("---------------\n");
-    printf("\n\n");
+    printf("\n---------------\n\n\n");
 
     if (LEN < LEN_MIN || LEN > LEN_MAX || PROB < PROB_MIN || PROB > PROB_MAX) {
         return 0;
@@ -92,131 +70,81 @@ int main() {
         int r1 = rand() % 101;
         int r2 = rand() % 101;
         printf("\n");
-        //printf("%d ", r1);
-        //printf("%d", r2);
 
 
         //Zturn 홀수번째
         Zturn = !Zturn;
 
         //C상태
-        if (PROB < r1) {
+        if (PROB < r1)
             C--;
-        }
 
         //Z상태
-        if (PROB > r2 && Zturn) {
+        if (PROB > r2 && Zturn)
             Z--;
-        }
         printf("\n");
 
-        //기차 상단
-        for (int i = 0; i < LEN; i++) {
+        //기차
+        for (int i = 0; i < LEN; i++)
             printf("#");
-        }
-        printf("\n");
-
-        //기차 중단
-        printf("#");
-
-        //C위치
-        for (int i = 0; i < C - 1; i++) {
+        printf("\n#");
+        for (int i = 0; i < C - 1; i++)
             printf(" ");
-        }
         printf("C");
-
-        //Z위치
-        for (int i = C; i < Z - 1; i++) {
+        for (int i = C; i < Z - 1; i++)
             printf(" ");
-        }
         printf("Z");
-
-        //M위치
-        for (int i = Z; i < M - 1; i++) {
+        for (int i = Z; i < M - 1; i++)
             printf(" ");
-        }
         printf("M");
-
-
-        //우측#위치
-        for (int i = 0; i <= LEN - M - 4; i++) {
+        for (int i = 0; i <= LEN - M - 3; i++)
             printf(" ");
-        }
         printf("#\n");
-
-        //기차 하단
-        for (int i = 0; i < LEN; i++) {
+        for (int i = 0; i < LEN; i++)
             printf("#");
-        }
-        printf("\n");
-        printf("\n");
+        prinf("\n\n");
+
         //C상태
-        if (PROB < r1) {
+        if (PROB < r1)
             printf("citizen: %d -> %d\n", C + 1, C);
-        }
-        else {
+        else
             printf("citizen: stay %d\n", C);
-        }
 
         //Z상태
-        if (PROB > r2 && Zturn) {
+        if (PROB > r2 && Zturn)
             printf("zombie: %d -> %d", Z + 1, Z);
-        }
-        else {
+        else
             printf("zombie: stay %d", Z);
-        }
 
         //Z쿨타임
-        if (!Zturn) {
+        if (!Zturn)
             printf(" (cannot move)\n");
-        }
-        else {
+        else
             printf("\n");
-        }
         printf("\n");
         printf("---------------\n");
         printf("\n");
 
         //Citizen won
         if (C == 1) {
-            //기차 상단
-            for (int i = 0; i < LEN; i++) {
+            //기차
+            for (int i = 0; i < LEN; i++)
                 printf("#");
-            }
-            printf("\n");
-
-            //기차 중단
-            printf("#");
-
-            //C위치
-            for (int i = 0; i < C - 1; i++) {
+            printf("\n#");
+            for (int i = 0; i < C - 1; i++)
                 printf(" ");
-            }
             printf("C");
-
-            //Z위치
-            for (int i = C; i < Z - 1; i++) {
+            for (int i = C; i < Z - 1; i++)
                 printf(" ");
-            }
             printf("Z");
-
-            //M위치
-            for (int i = Z; i < M - 1; i++) {
+            for (int i = Z; i < M - 1; i++)
                 printf(" ");
-            }
             printf("M");
-
-
-            //우측#위치
-            for (int i = 0; i <= LEN - M - 4; i++) {
+            for (int i = 0; i <= LEN - M - 3; i++)
                 printf(" ");
-            }
             printf("#\n");
-
-            //기차 하단
-            for (int i = 0; i < LEN; i++) {
+            for (int i = 0; i < LEN; i++)
                 printf("#");
-            }
 
 
             printf("\n");
@@ -226,44 +154,24 @@ int main() {
         }
         //Zombie won
         if (C + 1 == Z) {
-            //기차 상단
-            for (int i = 0; i < LEN; i++) {
+            //기차
+            for (int i = 0; i < LEN; i++)
                 printf("#");
-            }
-            printf("\n");
-
-            //기차 중단
-            printf("#");
-
-            //C위치
-            for (int i = 0; i < C - 1; i++) {
+            printf("\n#");
+            for (int i = 0; i < C - 1; i++)
                 printf(" ");
-            }
             printf("C");
-
-            //Z위치
-            for (int i = C; i < Z - 1; i++) {
+            for (int i = C; i < Z - 1; i++)
                 printf(" ");
-            }
             printf("Z");
-
-            //M위치
-            for (int i = Z; i < M - 1; i++) {
+            for (int i = Z; i < M - 1; i++)
                 printf(" ");
-            }
             printf("M");
-
-
-            //우측#위치
-            for (int i = 0; i <= LEN - M - 4; i++) {
+            for (int i = 0; i <= LEN - M - 3; i++)
                 printf(" ");
-            }
             printf("#\n");
-
-            //기차 하단
-            for (int i = 0; i < LEN; i++) {
+            for (int i = 0; i < LEN; i++)
                 printf("#");
-            }
             printf("\n");
             printf("GAME OVER!\n");
             printf("Citizen(s) has(have) been attacked by a zombie\n");
